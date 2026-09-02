e03ef3704f4a0b8d59129e871a1259e7eaede2b0
johnjameshickey <38435939+johnjameshickey@users.noreply.github.com>
2026-09-02 22:32:43 +0100

    Add Medical Log constants

---
+++ custom_components/medical_log/const.py
@@ -0,0 +1,13 @@
+DOMAIN = "medical_log"
+PLATFORMS = ["number", "sensor", "button"]
+
+CONF_CHILD_NAME = "child_name"
+CONF_MEDICATION_1 = "medication_1"
+CONF_MEDICATION_2 = "medication_2"
+
+DEFAULT_MEDICATION_1 = "Medication 1"
+DEFAULT_MEDICATION_2 = "Medication 2"
+
+STORAGE_VERSION = 1
+STORAGE_KEY_PREFIX = "medical_log"
+EVENT_LOG_ENTRY = "medical_log_entry"
